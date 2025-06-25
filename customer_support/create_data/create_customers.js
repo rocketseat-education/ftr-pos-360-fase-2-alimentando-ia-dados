57df420f9edc5df2360ca8475ffe90a87d3f936f
Lincoln Santana <62919518+lincolnsantana@users.noreply.github.com>
2025-06-25 18:22:43 -0300

    Aula 11: Criando dados de compras

--- customer_support/create_data/create_customers.js
+++ customer_support/create_data/create_customers.js
@@ -15,15 +15,47 @@ function createCustomer() {
     }
 }
 
+const customer = createCustomer();
 
-const purchaseProbability = [
-    { value: 0, weight: 10 },
-    { value: 1, weight: 50 },
-    { value: 2, weight: 20 },
-    { value: 3, weight: 10 },
-    { value: 4, weight: 7 },
-    { value: 5, weight: 3 }
-]
+function createPurchases(customer) {
+    const purchaseProbability = [
+        { value: 0, weight: 10 },
+        { value: 1, weight: 50 },
+        { value: 2, weight: 20 },
+        { value: 3, weight: 10 },
+        { value: 4, weight: 7 },
+        { value: 5, weight: 3 }
+    ];
 
+    const nPurchases = faker.helpers.weightedArrayElement(purchaseProbability);
 
-console.log(faker.helpers.weightedArrayElement(purchaseProbability));
+    const status = [
+        "confirmada",
+        "pagamento confirmado",
+        "em separação",
+        "em transito",
+        "entregue",
+        "atrasada",
+        "cancelado pelo usuário",
+        "cancelado pelo vendedor"
+    ];
+
+    const purchases = [];
+
+    for (let i = 0; i < nPurchases; i++) {
+        purchases.push(
+            {
+                customerId: customer.id,
+                product: faker.commerce.productName(),
+                price: faker.commerce.price(),
+                date: faker.date.recent({ days: 10 }),
+                status: faker.helpers.arrayElement(status)
+            }
+        );
+    }
+
+    return purchases;
+}
+
+console.log(createPurchases(customer));
+
