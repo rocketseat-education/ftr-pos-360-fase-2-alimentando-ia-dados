a05d910b70a5af564f2d3e4bd0fbd9d680c5c5cd
Lincoln Santana <62919518+lincolnsantana@users.noreply.github.com>
2025-06-25 17:51:43 -0300

    Aula 10: Criando distribuição de compras

--- customer_support/create_data/create_customers.js
+++ customer_support/create_data/create_customers.js
@@ -1,10 +1,29 @@
 import { fakerPT_BR as faker } from '@faker-js/faker';
 
-const firstName = faker.person.firstName();
-const lastName = faker.person.lastName();
+function createCustomer() {
 
-console.log(firstName);
-console.log(lastName);
-console.log(faker.date.birthdate());
-console.log(faker.location.state());
-console.log(faker.internet.email({ firstName:  firstName, lastName: lastName }));
+    const firstName = faker.person.firstName();
+    const lastName = faker.person.lastName();
+
+    return {
+        id: faker.string.uuid(),
+        firstName: firstName,
+        lastName: lastName,
+        birthDate: faker.date.birthdate(),
+        state: faker.location.state(),
+        email: faker.internet.email({ firstName: firstName, lastName: lastName })
+    }
+}
+
+
+const purchaseProbability = [
+    { value: 0, weight: 10 },
+    { value: 1, weight: 50 },
+    { value: 2, weight: 20 },
+    { value: 3, weight: 10 },
+    { value: 4, weight: 7 },
+    { value: 5, weight: 3 }
+]
+
+
+console.log(faker.helpers.weightedArrayElement(purchaseProbability));
