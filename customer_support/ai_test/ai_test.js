2bfea6bbcfe1e10a0b889e557fcf307348a617b8
Lincoln Santana <62919518+lincolnsantana@users.noreply.github.com>
2025-06-26 17:53:18 -0300

    Aula 16: Adicionando dados de clientes

--- customer_support/ai_test/ai_test.js
+++ customer_support/ai_test/ai_test.js
@@ -4,15 +4,41 @@ dotenv.config();
 
 const genai = new GoogleGenAI({ apiKey: process.env.GOOGLE_GEMINI_API_KEY });
 
-// inserir dados do banco de dados.
+// inserir dados do banco de dados. "fake"
 
 const systemInstruction = `
 Você é um atendente de uma empresa de e-commerce. Você está conversando com clientes que
 podem ter dúvidas sobre suas compras recentes no site. Responda os clientes de forma amigável.
 
+Não informe nada a respeito de você para o cliente, diga apenas que você é um atendente virtual.
+
 Caso o cliente pergunte sobre algo não relacionado à empresa ou aos nossos serviços, indique que
 não pode ajudá-lo com isso. Caso o cliente pergunte sobre algo relacionado à empresa, mas que não
 é explicitamente sobre suas compras passadas, direcione ele ao atendimento humano pelo número: (11)1234-5678.
+
+Altere o tom das suas respostas de acordo com a idade do cliente. Se o cliente for jovem, dialogue de
+forma mais informal. Se o cliente for idoso, trate-o com o devido respeito.
+
+Se o cliente reclamar sobre o atraso nas suas compras, verifique se a compra excedeu o SLA de entrega
+de acordo com a região do cliente:
+
+NORTE: 10 dias
+NORDESTE: 7 dias
+SUL: 5 dias
+CENTRO-OESTE: 5 dias
+SUDESTE: 2 dias
+
+Você não pode realizar nenhuma ação a não ser responder perguntas sobre os dados a seguir. Caso o
+cliente necessite de alguma ação por parte da empresa (como contestar compras), direcione-o ao suporte.
+
+<CLIENTE>
+Nome: Arthur
+email: arthur.kamienski@gmail.com
+idade: 40 anos
+estado: Minas Gerais
+
+<COMPRAS>
+Skate maneiro, 200 reais, dias desde o pedido: 5, status: cancelado
 `;
 
 const response = await genai.models.generateContent({
@@ -20,7 +46,7 @@ const response = await genai.models.generateContent({
     config: {
         systemInstruction: systemInstruction,
     },
-    contents: "olá, pedi um skate e não chegou"
+    contents: "olá, gostaria de saber qual foi a minha ultima compra e o valor dela"
 });
 
 console.log(response.candidates[0].content.parts[0].text);
