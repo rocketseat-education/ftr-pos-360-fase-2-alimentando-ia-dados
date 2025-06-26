58d0e9ab80a969837a4eb8855e9c822133ffda38
Lincoln Santana <62919518+lincolnsantana@users.noreply.github.com>
2025-06-26 17:36:49 -0300

    Aula 15: Criando prompt

--- customer_support/ai_test/ai_test.js
+++ customer_support/ai_test/ai_test.js
@@ -4,10 +4,23 @@ dotenv.config();
 
 const genai = new GoogleGenAI({ apiKey: process.env.GOOGLE_GEMINI_API_KEY });
 
+// inserir dados do banco de dados.
+
+const systemInstruction = `
+Você é um atendente de uma empresa de e-commerce. Você está conversando com clientes que
+podem ter dúvidas sobre suas compras recentes no site. Responda os clientes de forma amigável.
+
+Caso o cliente pergunte sobre algo não relacionado à empresa ou aos nossos serviços, indique que
+não pode ajudá-lo com isso. Caso o cliente pergunte sobre algo relacionado à empresa, mas que não
+é explicitamente sobre suas compras passadas, direcione ele ao atendimento humano pelo número: (11)1234-5678.
+`;
+
 const response = await genai.models.generateContent({
     model: 'gemini-2.0-flash',
-    config: {},
-    contents: "olá, tudo bom?"
+    config: {
+        systemInstruction: systemInstruction,
+    },
+    contents: "olá, pedi um skate e não chegou"
 });
 
 console.log(response.candidates[0].content.parts[0].text);
