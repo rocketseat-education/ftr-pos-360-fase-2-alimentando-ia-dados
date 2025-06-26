35a3bb1222c9efe5d161f7954b4867b03ca1a7a7
Lincoln Santana <62919518+lincolnsantana@users.noreply.github.com>
2025-06-26 14:25:56 -0300

    Aula 12: Criando tabelas no postgreSQL

--- customer_support/create_data/create_customers.js
+++ customer_support/create_data/create_customers.js
@@ -1,4 +1,5 @@
 import { fakerPT_BR as faker } from '@faker-js/faker';
+import { Pool } from 'pg';
 
 function createCustomer() {
 
@@ -14,8 +15,6 @@ function createCustomer() {
         email: faker.internet.email({ firstName: firstName, lastName: lastName })
     }
 }
-
-const customer = createCustomer();
 
 function createPurchases(customer) {
     const purchaseProbability = [
@@ -57,5 +56,65 @@ function createPurchases(customer) {
     return purchases;
 }
 
-console.log(createPurchases(customer));
 
+let purchases = [];
+
+const customers = [];
+
+for (let i=0; i<100; i++) {
+    customers.push(createCustomer());
+}
+
+for(let customer of customers){
+    purchases = purchases.concat(createPurchases(customer));
+}
+
+const pool = new Pool({
+    host: 'localhost',
+    user: 'postgres',      // coloque o usuário do seu banco
+    password: 'root',    // coloque a senha do seu banco
+    database: 'customer-chat'
+});
+
+await pool.query(`DROP TABLE IF EXISTS purchases;`);
+await pool.query(`DROP TABLE IF EXISTS customers;`);
+await pool.query(`DROP TYPE IF EXISTS purchase_status;`);
+
+await pool.query(`
+    CREATE TABLE customers (
+    id UUID PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    birth_date DATE NOT NULL,
+    state VARCHAR(100),
+    email VARCHAR(255) UNIQUE NOT NULL
+);
+`);
+
+await pool.query(`
+    CREATE TYPE purchase_status AS ENUM (
+    'confirmada',
+    'pagamento confirmado',
+    'em separação',
+    'em transito',
+    'entregue',
+    'atrasada',
+    'cancelado pelo usuário',
+    'cancelado pelo vendedor'
+);
+`);
+
+await pool.query(`
+    CREATE TABLE purchases (
+        id UUID PRIMARY KEY,
+        customer_id UUID NOT NULL,
+        product VARCHAR(255) NOT NULL,
+        price NUMERIC(10, 2) NOT NULL,
+        date DATE NOT NULL,
+        status purchase_status NOT NULL,
+        CONSTRAINT fk_customer
+            FOREIGN KEY (customer_id)
+            REFERENCES customers(id)
+            ON DELETE CASCADE
+);
+`);
