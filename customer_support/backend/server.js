2db2bac7f43245811855c85d0ddab2071a05c8c2
Lincoln Santana <62919518+lincolnsantana@users.noreply.github.com>
2025-06-30 14:08:47 -0300

    Aula 21: Adicionando memória ao chat

--- customer_support/backend/server.js
+++ customer_support/backend/server.js
@@ -9,14 +9,30 @@ app.use(express.json());
 
 const port = 3000;
 
+const chatHistory = {};
+
 app.post("/support", async (req, res) => {
     const email = req.body.email;
     const message = req.body.message;
     
     //const email = "Silas_Silva@bol.com.br";
 
+    if (!(email in chatHistory)){
+        chatHistory[email] = []
+    }
+
+    chatHistory[email].push({
+        role: "user",
+        parts: [{ text: message }] 
+    });
+
     const customerInfo = await getCustomerInfo(email);
-    const response = await getAIResponse(customerInfo, message);
+    const response = await getAIResponse(customerInfo, chatHistory[email]);
+
+    chatHistory[email].push({
+        role: "model",
+        parts: [{ text: response }] 
+    });
 
     res.send(JSON.stringify({
         response: response
