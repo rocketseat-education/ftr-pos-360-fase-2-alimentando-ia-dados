de6e19bdcb19407fee20fc69b96fbda301e9ff1a
Lincoln Santana <62919518+lincolnsantana@users.noreply.github.com>
2025-06-30 14:42:04 -0300

    Aula 23: Estilizando caixa de login

--- customer_support/chat/src/components/LoginBox/LoginBox.jsx
+++ customer_support/chat/src/components/LoginBox/LoginBox.jsx
@@ -1,13 +1,25 @@
+import { useRef } from "react";
 import "./LoginBox.css";
 
-export default function Loginbox({ setEmail }){
-    function login(){
-        setEmail("Silas_Silva@bol.com.br");
+export default function Loginbox({ setEmail }) {
+    const emailInput = useRef(null);
+
+    function login() {
+        const email = emailInput.current.value;
+
+        if (email.length === 0) {
+            return;
+        }
+
+        setEmail(email);
     }
     return <>
-    <div>
-        <input></input>
-        <button onClick={login}>Login</button>
-    </div>
+        <div class="login-background">
+            <div className="login-div">
+                <input className="login-input" ref={emailInput}></input>
+                <button className="login-button" onClick={login}>Login</button>
+            </div>
+        </div>
+
     </>
 }
