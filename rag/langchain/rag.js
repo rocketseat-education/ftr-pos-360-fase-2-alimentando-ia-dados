471dccddedddadbdab2a679b0498e5e4a794231f
Lincoln Santana <62919518+lincolnsantana@users.noreply.github.com>
2025-07-03 09:34:00 -0300

    Aula 32: Criando vector database

--- rag/langchain/rag.js
+++ rag/langchain/rag.js
@@ -1,5 +1,9 @@
 import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import { Chroma } from "@langchain/community/vectorstores/chroma";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const url = "https://eloquentjavascript.net/1st_edition/print.html";
 const cheerioLoader = new CheerioWebBaseLoader(
@@ -16,3 +20,15 @@ const splitter = new RecursiveCharacterTextSplitter({
 const allSplits = await splitter.splitDocuments(docs);
 
 console.log(allSplits);
+
+const embeddings = new GoogleGenerativeAIEmbeddings({
+    apiKey: process.env.GOOGLE_GEMINI_API_KEY,
+    model: "text-embedding-004",
+    taskType: "RETRIEVAL_DOCUMENT"
+});
+
+const vectorStore = new Chroma(embeddings, {
+    collectionName:"javascript-book-gemini-embeddings"
+});
+
+// vectorStore.addDocuments(allSplits);
